833156e0dcddf4cca6ce606e5764d438159187b3
zombiradiactivo <andres11geminis@gmail.com>
2026-03-05 13:39:22 +0100

    Update calculadora_propinas.py
    Commit 3
    Porcentaje personalizado y validacion de porcentaje positivo

--- calculadora-propinas/src/calculadora_propinas.py
+++ calculadora-propinas/src/calculadora_propinas.py
@@ -1,7 +1,23 @@
-def calcular_propina_10_fijo(cuenta: float):
-    propina = cuenta * 0.1
-    return propina
+def calcular_propina(cuenta: float, porcentaje):
+
+    if porcentaje > 0:
+        porcentaje = porcentaje / 100
+        print(porcentaje)
+        propina = cuenta * porcentaje
+        return propina
+    else:
+        print("El porcentaje no puede ser negativo")
 
 ## Test
 
-print("La propina fija del 10% es: ",calcular_propina_10_fijo(100),"€")
+
+def testing():
+
+    print("Introduce la cuenta:")
+    cuenta = float(input())
+    print("Introduce el porcentaje de la propina:")
+    porcentaje = float(input())
+
+    print(f"La propina de porcentaje {porcentaje}% es: ",calcular_propina(cuenta,porcentaje),"€")
+
+testing()
